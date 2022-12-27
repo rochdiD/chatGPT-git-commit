5061a81b91d947c9b92c0db0cfd0456d503f5210
Rochdi <rochdi.plane@gmail.com>
2022-12-27 19:59:03 +0100

    Update GPT_commit.clj

--- GPT_commit.clj
+++ GPT_commit.clj
@@ -1,14 +1,28 @@
 (ns GPT-commit.core
-  (:require [clojure.java.shell :as shell]))
+  (:require [clojure.java.shell :as shell]
+            [clj-http.client :as client]
+            [clojure.data.json :as json]
+            [chatgpt.core :as chatgpt]))
 
-(defn list-changed-files []
-  (-> (shell/sh "git" "diff" "--name-only")
+(defn list-new-and-modified-files []
+  (-> (shell/sh "git" "ls-files" "--others" "--modified")
       :out
       (clojure.string/split-lines)))
+
+(defn get-diff [file]
+  (-> (shell/sh "git" "diff" file)
+      :out
+      clojure.string/trim))
 
 (defn commit-file [file message]
   (shell/sh "git" "commit" "-m" message file))
 
-(defn commit-changed-files [message]
-  (doseq [file (list-changed-files)]
-    (commit-file file message)))
+(defn summarize-diff [file]
+  (let [diff (get-diff file)
+        prompt (str "Summarize the diff of this file: " diff)]
+    (-> (chategpt/get-response prompt)
+        :message)))
+
+(defn commit-new-and-modified-files []
+  (doseq [file (list-new-and-modified-files)]
+    (commit-file file (summarize-diff file)))
