e05c1ffceaa5ea7a585575d5a82bb92e9decea25
Rochdi <rochdi.nouaiti@gmail.com>
2022-12-29 22:46:21 +0100

    adjust request body and paramaters as in API doc

--- src/chatgpt_api/core.clj
+++ src/chatgpt_api/core.clj
@@ -3,26 +3,32 @@
             [clojure.data.json :as json]))
 
 (def api-key "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")
-(def chatgpt-api-url "https://api.openai.com/v1/models/text-davinci-003")
+(def chatgpt-api-url "https://api.openai.com/v1/completions")
 
-(#_(defn build-request-data [prompt]
+(defn build-request-data [prompt]
   {:model "text-davinci-003"
-   }))
+   :prompt prompt
+   :max_tokens 30
+   :temperature 0.3
+   })
 
 (defn build-request-headers []
-  {"Authorization" (str "Bearer " api-key)})
+  {"Content-Type" "application/json"
+   "Authorization" (str "Bearer " api-key)})
 
-(defn send-request [headers]
-  (let [response (client/get chatgpt-api-url
+(defn send-request [headers data]
+  (let [response (client/post chatgpt-api-url
                               {:headers headers
+                               :body (json/write-str data)
                                :as :string
                                :socket-timeout 5000})]
-    (println (:body response) :message)
-    (:body response) :message))
+    (println (:body response) )
+    (:body response)))
 
 (defn ask-chatgpt [prompt]
-  (let [headers (build-request-headers)]
-    (send-request  headers)))
+  (let [headers (build-request-headers)
+        data (build-request-data prompt)]
+    (send-request headers data)))
 
 
 (defn -main []
